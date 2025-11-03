20bf3d966684d0737e7a9a7b21d7e2bfd992a2ac
Marufa Akter <marufaakter1947@gmail.com>
2025-11-03 21:23:39 +0600

    Creating DELETE API for removing specific data

--- index.js
+++ index.js
@@ -69,17 +69,30 @@ async function run() {
       const data = req.body;
       // console.log(id)
       // console.log(data);
-      const objectId = new ObjectId(id)
-      const filter = {_id: objectId}
+      const objectId = new ObjectId(id);
+      const filter = { _id: objectId };
       const update = {
-        $set: data
-      }
-      const result = await modelCollection.updateOne(filter,update);
+        $set: data,
+      };
+      const result = await modelCollection.updateOne(filter, update);
 
       res.send({
         success: true,
         result,
       });
+    });
+
+    // Delete
+    // deleteOne
+    // deleteMany
+    app.delete("/models/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await modelCollection.deleteOne({ _id: new ObjectId(id) });
+
+      res.send({
+        success:true,
+        result,
+      })
     });
 
     await client.db("admin").command({ ping: 1 });
